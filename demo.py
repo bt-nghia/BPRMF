e3fe59fecdbb4f81d02cf8fc418b9607a1fac9e1
bt-nghia <bnghia7303@gmail.com>
2025-02-26 16:36:09 +0700

    add all

--- demo.py
+++ demo.py
@@ -1,11 +1,22 @@
 import torch
+import pandas as pd
+import numpy as np
 
 topk = 10
 
-uids = [1]
+uids = [1] # single user only
 uids = torch.tensor(uids)
 
 model = torch.load("weight.pth")
 score = model.pred(uids)
 ranking_score, rec_ids = torch.topk(score, k=topk, dim=1)
 print(rec_ids)
+
+movies = pd.read_csv("ml-1m/movies.dat", sep="::", encoding = "ISO-8859-1", names=["id", "name", "cates"])
+# print(movies[movies["id"] == 1])
+movies = movies[["id", "name"]]
+
+for i in range(len(uids)):
+    print("Rec film for user", uids[i])
+    for iid in rec_ids[i]:
+        print(movies[movies["id"] == int(iid)])
